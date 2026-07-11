5361fd5e81c12833f5e58b993d714fb87df7f8c0
vincenzopanes <vpanesirribarra@byupathway.edu>
2026-07-11 19:58:54 -0400

    uno mas

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -3,7 +3,7 @@ using System;
 class Program
 {
     static void Main(string[] args)
-        {
+    {
         DisplayWelcome();
 
         string userName = PromptUserName();
@@ -13,5 +13,32 @@ class Program
         int squaredNumber = SquareNumber(favoriteNumber);
 
         DisplayResult(userName, squaredNumber);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string name, int squaredNumber)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
     }
 }
